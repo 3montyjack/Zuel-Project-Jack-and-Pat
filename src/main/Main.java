023c2fb6d2117846163dd170efb3cc618697cc0d
3montyjack <3montyjack@gmail.com>
2017-03-02 16:48:10 -0500

    Initialized
    This is a project file for eclipse

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+	
+	static Game game;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		game = new Game();
+		game.play();
+	}
+
+}
