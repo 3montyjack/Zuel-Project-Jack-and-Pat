019ff66de07efa7e03af50229ac6c69b3ebd495b
3montyjack <3montyjack@gmail.com>
2017-03-22 18:43:52 -0400

    Finished room to make it better

--- src/main/ItemList.java
+++ src/main/ItemList.java
@@ -18,7 +18,8 @@
 			list.add(new Item("dagger", 10, 3, "weapon", "A small dagger. There is an engraving on the handle in\n"
 					+ " a language you don't recognize.", true));
 			list.add(new Item("crate", 150, 0, "storage", "A sealed wooden box. What do goblins have to store?", false));
-			
+			list.add(new Item("bat", 10, 2, "weapon", "A small piece of wood rounded arround the edges", false));
+			list.add(new Item("teleporter", 0, 0, "object","A mysterious piece of telepotation technology", false));
 			
 		}
 	
